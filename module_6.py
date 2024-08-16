246232d98c755f0fecbe932a7c7d4cfa0d0c269d
xLime93 <pylypenko.o@icloud.com>
2024-08-16 12:11:31 +0300

    Add files via upload

---
+++ module_6.py
@@ -0,0 +1,103 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+    
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Імʼя - обовʼязкове поле")
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Телефон повинен містити 10 цифр")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone_number):
+        phone = Phone(phone_number)
+        self.phones.append(phone)
+
+    def remove_phone(self, phone_number):
+        phone_to_remove = self.find_phone(phone_number)
+        if phone_to_remove:
+            self.phones.remove(phone_to_remove)
+        else:
+            raise ValueError("Номер телефону не знайдено")
+
+    def edit_phone(self, old_phone_number, new_phone_number):
+        phone_to_edit = self.find_phone(old_phone_number)
+        if phone_to_edit:
+            self.remove_phone(old_phone_number)
+            self.add_phone(new_phone_number)
+        else:
+            raise ValueError("Номер телефону не знайдено")
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    def __str__(self):
+        phones_str = "; ".join(phone.value for phone in self.phones)
+        return f"Імʼя: {self.name.value}, телефон(и): {phones_str}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            raise ValueError("Запис не знайдено")
+
+    def __str__(self):
+        return "\n".join(str(record) for record in self.data.values())
+
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+    
+print(book)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
